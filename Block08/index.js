17c9b23ba2b4dedef72e9dd78568fa05ca944173
khetfield <163695693+khetfield@users.noreply.github.com>
2024-04-15 15:32:22 -0400

    Java upload
    Java upload

---
+++ Block08/index.js
@@ -0,0 +1,8 @@
+// creating variables
+const combination1 = "5 x 2";
+const combination2 = " 40 / 2";
+const combination3 = "100-61";
+
+// create alert
+const notification = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:\n"
+alert(notification+" \nCombination 1: "+combination1+"\nCombination 2: "+combination2+"\nCombination 3: "+combination3);
